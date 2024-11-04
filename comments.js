14866bd6b5efc8033bf88c61b9bac3e13b167b39
Prasanna Vengatesh V <prasannavengatesh@karunya.edu.in>
2024-11-04 16:44:28 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,28 @@
+//create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+const port = 3000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//get all comments
+app.get('/comments', (req, res) => {
+    fs.readFile('./comments.json', (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
+});
+
+//add comment
+app.post('/comments', (req, res) => {
+    fs.readFile('./comments.json', (err, data) => {
+        if (err) throw err;
+
+        const comments = JSON.parse(data);
+        const newComment = {
+            id: comments.length + 1,
